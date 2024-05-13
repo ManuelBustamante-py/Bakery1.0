9c3f8dcd5bf8040fe1cadbd87ccb345596b44092
ManuelBustamante <man.bustamante@duocuc.cl>
2024-05-12 20:35:24 -0400

    octava actualización

--- js/main.js
+++ js/main.js
@@ -51,12 +51,7 @@
         ]
     });
 
-    // Facts counter
-    $('[data-toggle="counter-up"]').counterUp({
-        delay: 10,
-        time: 2000
-    });
-
+ 
     // Testimonials carousel
     $(".testimonial-carousel").owlCarousel({
         autoplay: false,
